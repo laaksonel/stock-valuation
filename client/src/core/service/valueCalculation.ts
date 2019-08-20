e156cc238b1b9c8e0449ee8019953affbc484f21
Lauri Laaksonen <lauri.k.laaksonen@gmail.com>
2019-08-20 21:29:17 +0300

    First draft of value calculator in UI

---
+++ client/src/core/service/valueCalculation.ts
@@ -0,0 +1,29 @@
+import { StockData } from "../../domain/stock-valuation/stock.reducer";
+import { StockValuationParams } from "../../domain/stock-valuation/StockValuationPage";
+
+export interface StockValuationResult {
+  valueInFiveYears: number;
+  todayIntrinsicValue: number;
+}
+
+export function calculateValuation(valuationInputs: StockValuationParams) {
+  const {
+    valuationData,
+    multipliers
+  } = valuationInputs;
+
+  const {
+    eps,
+    fiveYearPE,
+    expectedGrowth
+  } = valuationData;
+
+  const growthWithMarginOfSafety = expectedGrowth * (1 - multipliers.marginOfSafety);
+  const asFiveYearGrowth = Math.pow(growthWithMarginOfSafety, 5);
+  const result = fiveYearPE * eps * asFiveYearGrowth;
+
+  return {
+    valueInFiveYears: result,
+    todayIntrinsicValue: result / Math.pow(1 + multipliers.discount, 5)
+  }
+}
